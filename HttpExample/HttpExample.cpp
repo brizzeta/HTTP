5d566737bfbd5ddad6be9e75b8222e1608d668e4
brizzeta <76489220+brizzeta@users.noreply.github.com>
2022-12-24 14:56:50 +0200

    Add files via upload

---
+++ HttpExample/HttpExample.cpp
@@ -0,0 +1,220 @@
+﻿#pragma comment (lib, "Ws2_32.lib")
+#include <Winsock2.h>
+#include <ws2tcpip.h>
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{  
+    while (1)
+    {
+        //1. инициализация "Ws2_32.dll" для текущего процесса
+        WSADATA wsaData;
+        WORD wVersionRequested = MAKEWORD(2, 2);
+
+        int err = WSAStartup(wVersionRequested, &wsaData);
+        if (err != 0) {
+
+            cout << "WSAStartup failed with error: " << err << endl;
+            return 1;
+        }
+
+        //инициализация структуры, для указания ip адреса и порта сервера с которым мы хотим соединиться
+
+        char hostname[255] = "api.openweathermap.org";
+
+        addrinfo* result = NULL;
+
+        addrinfo hints;
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        int iResult = getaddrinfo(hostname, "http", &hints, &result);
+        if (iResult != 0) {
+            cout << "getaddrinfo failed with error: " << iResult << endl;
+            WSACleanup();
+            return 3;
+        }
+
+        SOCKET connectSocket = INVALID_SOCKET;
+        addrinfo* ptr = NULL;
+
+        //Пробуем присоединиться к полученному адресу
+        for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+            //2. создание клиентского сокета
+            connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+            if (connectSocket == INVALID_SOCKET) {
+                printf("socket failed with error: %ld\n", WSAGetLastError());
+                WSACleanup();
+                return 1;
+            }
+
+            //3. Соединяемся с сервером
+            iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+            if (iResult == SOCKET_ERROR) {
+                closesocket(connectSocket);
+                connectSocket = INVALID_SOCKET;
+                continue;
+            }
+            break;
+        }
+
+    
+        //4. HTTP Request
+        string country;
+        cout << "Enter country: ";
+        cin >> country;
+
+        string uri = "/data/2.5/weather?q=" + country + "&appid=75f6e64d49db78658d09cb5ab201e483&mode=JSON";
+
+        string request = "GET " + uri + " HTTP/1.1\n";
+        request += "Host: " + string(hostname) + "\n";
+        request += "Accept: */*\n";
+        request += "Accept-Encoding: gzip, deflate, br\n";
+        request += "Connection: close\n";
+        request += "\n";
+
+        //отправка сообщения
+        if (send(connectSocket, request.c_str(), request.length(), 0) == SOCKET_ERROR) {
+            cout << "send failed: " << WSAGetLastError() << endl;
+            closesocket(connectSocket);
+            WSACleanup();
+            return 5;
+        }
+        cout << "send data" << endl;
+
+        //5. HTTP Response
+
+        string response;
+
+        const size_t BUFFERSIZE = 1024;
+        char resBuf[BUFFERSIZE];
+
+        int respLength;
+
+        do {
+            respLength = recv(connectSocket, resBuf, BUFFERSIZE, 0);
+            if (respLength > 0) {
+                response += string(resBuf).substr(0, respLength);
+            }
+            else {
+                cout << "recv failed: " << WSAGetLastError() << endl;
+                closesocket(connectSocket);
+                WSACleanup();
+                return 6;
+            }
+
+        } while (respLength == BUFFERSIZE);        
+
+        static int index = 0;   //количество запросов
+        index++;                //увеличиваем
+
+        char out[][15] = {
+            "id",
+            "name",
+            "country",
+            "lon",
+            "lat",
+            "temp_min",
+            "temp_max",
+            "sunrise",
+            "sunset"
+        };
+        char in[9][20];        
+
+        //поиск данных
+        for (int i = 0; i < 9; i++)
+        {
+            int lenght = 0;
+            int i_out = 0;
+            int i_in = 0;
+            while (lenght != response.size())
+            {
+                if (out[i][i_out] == response[lenght])
+                {
+                    i_out++;
+                }
+                else if (response[lenght] == '"' && i_out == strlen(out[i]))
+                {
+                    while (response[lenght + 2] != ',')
+                    {
+                        in[i][i_in] = response[lenght + 2];
+                        i_in++;
+                        lenght++;
+                    }
+                    if (in[i][i_in - 1] == '}')
+                    {
+                        in[i][i_in - 1] = '\0';
+                    }
+                    else in[i][i_in] = '\0';
+
+                    i_in = 0;
+                    break;
+                }
+                else { i_out = 0; }
+                lenght++;
+            }
+            cout << out[i] << ": " << in[i] << endl;
+        }
+
+        //запись в файл        
+
+        ofstream out_f("history.txt", ios::app);
+        if (out_f.is_open())
+        {
+            out_f << index << "--------------------------------------\n";
+            for (int i = 0; i < 9; i++)
+            {
+                out_f << out[i] << ": " << in[i] << "\n";
+            }
+            out_f << "\n";
+        }
+        else cout << "Cannot open the file for writing.";
+        out_f.close();
+
+        cout << endl << "Get the history?(y/n) ";
+        char answ;
+        cin >> answ;
+
+        //получение данных из файла
+        if (answ == 'y')
+        {
+            cout << endl << "HISTORY" << endl;
+            ifstream in_f("history.txt");
+            string line;
+
+            if (in_f.is_open())
+            {
+                while (getline(in_f, line))
+                {
+                    cout << line << endl;
+                }
+                cout << endl;
+            }
+            else cout << "Cannot open the file for reading.";
+            in_f.close();
+        }
+
+        _getch();
+        system("cls");
+    
+
+        //отключает отправку и получение сообщений сокетом
+        iResult = shutdown(connectSocket, SD_BOTH);
+        if (iResult == SOCKET_ERROR) {
+            cout << "shutdown failed: " << WSAGetLastError() << endl;
+            closesocket(connectSocket);
+            WSACleanup();
+            return 7;
+        }
+
+        closesocket(connectSocket);
+        WSACleanup();
+    }
+}
